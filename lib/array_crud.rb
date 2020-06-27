154193626224ead09d19a06ae55d56c4b296b129
Joseph Perez <hello@josephdanielperez.com>
2020-06-27 04:33:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  "element1", "element2", "element3", "element4"]
+  "element1", "element2", "element3", "element4"
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
